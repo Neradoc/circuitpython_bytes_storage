689cbae651e402430c387cd12cd39e2b3a55a9f9
Neradoc <neraOnGit@ri1.fr>
2025-03-28 18:41:17 +0100

    add file storage

--- bytes_storage.py
+++ bytes_storage.py
@@ -19,7 +19,7 @@ class ByteStorage:
         msgpack.pack(data, packed_data_io)
         length = packed_data_io.tell()
 
-        if length + 8 > len(self.storage):
+        if length + 8 + self.offset > len(self.storage):
             raise ValueError("Data can't fit into storage facility")
 
         self.storage[self.offset:self.offset+8] = length.to_bytes(8, "big")
@@ -43,8 +43,8 @@ class ByteStorage:
         return len(self.storage)
 
 class ByteArrayStorage(ByteStorage):
-    def __init__(self, size:int, offset:int = 0):
-        super().__init__(bytearray(size), offset)
+    def __init__(self, size:int):
+        super().__init__(bytearray(size))
 
 class SleepMemoryStorage(ByteStorage):
     def __init__(self, offset:int = 0):
@@ -54,17 +54,42 @@ class NVMStorage(ByteStorage):
     def __init__(self, offset:int = 0):
         super().__init__(microcontroller.nvm, offset)
 
+class FileStorage():
+    def __init__(self, file):
+        self.file = file
+
+    def save(self, data):
+        packed_data_io = BytesIO()
+        msgpack.pack(data, packed_data_io)
+        length = packed_data_io.tell()
+
+        with open(self.file, "wb") as fp:
+            fp.write(packed_data_io.getvalue())
+
+    def read(self):
+        try:
+            with open(self.file, "rb") as fp:
+                data_s = fp.read()
+        except OSError:
+            return None
+
+        try:
+            data = msgpack.unpack(BytesIO(data_s))
+            return data
+        except ValueError:
+            return None
+
+    def __len__(self):
+        try:
+            stat = os.stat(self.file)
+            return stat[6]
+        except OSError:
+            return 0
 
 def _test():
     import alarm
     import microcontroller
     data = {"name": "nvm_helper", "num": 92, "float": 3.14}
-    storage = ByteStorage(bytearray(1024))
-    storage.save(data)
-    print(storage.read())
-    storage = ByteStorage(alarm.sleep_memory)
-    storage.save(data)
-    print(storage.read())
-    storage = ByteStorage(microcontroller.nvm)
+    storage = ByteArrayStorage(bytearray(1024))
     storage.save(data)
     print(storage.read())
